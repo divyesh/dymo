af7f8f8a0c5670f690185b895a00db0d291c4c9e
Kunal Chaudhari <kunalchaudhari@gmail.com>
2014-06-20 16:03:10 +0530

    fix bug while searching visit

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -3,6 +3,6 @@ class Visit < ActiveRecord::Base
   belongs_to :physician
   validates_presence_of :patient_id, :physician_id
   def self.search(search)
-    Patient.search(search)
+    joins(:patient).where('patients.firstname LIKE ? OR patients.lastname LIKE ? OR patients.middlename LIKE ? OR patients.healthnumber LIKE ?', "%#{search}%","%#{search}%","%#{search}%","%#{search}%")
   end
 end
