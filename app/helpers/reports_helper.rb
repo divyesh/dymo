940ccde1230af63cd5347ec62bb7296a270520c2
Kunal Chaudhari <kunalchaudhari@gmail.com>
2014-07-08 01:57:58 +0530

    fix waiting time

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -1,11 +1,11 @@
 module ReportsHelper
   def waiting_time(time_in_seconds)
-    if time_in_seconds > 60
-      time_in_seconds = (time_in_seconds/60).to_i
-      "#{time_in_seconds} minutes"
-    elsif time_in_seconds > 3600
+    if time_in_seconds > 3600
       time_in_seconds = (time_in_seconds/3600).to_i
       "#{time_in_seconds} hours"
+    elsif time_in_seconds > 60
+      time_in_seconds = (time_in_seconds/60).to_i
+      "#{time_in_seconds} minutes"
     else
       "#{time_in_seconds} seconds"
     end
