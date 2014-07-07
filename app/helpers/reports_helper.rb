24c18b68da6f42f2f982b22e34b571110b64de4c
Kunal Chaudhari <kunalchaudhari@gmail.com>
2014-07-08 01:50:39 +0530

    seconds. minute and hours

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -1,2 +1,13 @@
 module ReportsHelper
+  def waiting_time(time_in_seconds)
+    if time_in_seconds > 60
+      time_in_seconds = (time_in_seconds/60).to_i
+      "#{time_in_seconds} minutes"
+    elsif time > 3600
+      time_in_seconds = (time_in_seconds/3600).to_i
+      "#{time_in_seconds} hours"
+    else
+      "#{time_in_seconds} seconds"
+    end
+  end
 end
