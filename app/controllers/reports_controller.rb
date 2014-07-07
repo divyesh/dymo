0a5d3bda100d2eaef05087706ff2d53b5980a99b
Kunal Chaudhari <kunalchaudhari@gmail.com>
2014-07-08 01:42:31 +0530

    generate report for only completed token in first draft

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,6 +1,6 @@
 class ReportsController < ApplicationController
   def index
     date = filter_date
-    @tokens = Token.where("(created_at >= ? AND created_at <= ?) AND state = ?)", date.beginning_of_day, date.end_of_day, "completed")
+    @tokens = Token.where("(created_at >= ? AND created_at <= ?) AND (state = ?)", date.beginning_of_day, date.end_of_day, "completed")
   end
 end
