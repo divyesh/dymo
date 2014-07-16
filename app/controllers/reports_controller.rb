6edb905db0c67a40c99019878584ff2eda394446
Kunal Chaudhari <kunalchaudhari@gmail.com>
2014-07-16 23:20:14 +0530

    WIP: reports

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,5 +2,9 @@ class ReportsController < ApplicationController
   def index
     date = filter_date
     @tokens = Token.where("(created_at >= ? AND created_at <= ?) AND (state = ?)", date.beginning_of_day, date.end_of_day, "completed")
+
+    if params[:duration]
+      
+    end
   end
 end
