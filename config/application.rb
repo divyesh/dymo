b9e8d0a1c4461d94493e432fb6fc828b38e8e0d0
Kunal Chaudhari <kunalchaudhari@gmail.com>
2014-07-26 00:10:23 +0530

    add timezone

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,7 @@ module Dymo
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.precompile += %w( mobile.js mobile.css )
+    config.time_zone = 'Eastern Time (US & Canada)'
+    config.active_record.default_timezone = 'Eastern Time (US & Canada)'
   end
 end
