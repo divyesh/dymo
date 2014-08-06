a799b41d32af1a01043ee19cbf1168e96b956f9e
Kunal Chaudhari <kunalchaudhari@gmail.com>
2014-08-06 23:04:54 +0530

    normal user cant delete records

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,13 +7,12 @@ class Ability
     user ||= User.new # guest user (not logged in)
     if user.admin?
       can :manage, :all
-      can :access, :rails_admin   # grant access to rails_admin    
+      can :access, :rails_admin   # grant access to rails_admin
     else
       can :read, :all
       unless user.new_record?
         can :create, :all
         can :update, :all
-        can :destroy, :all
         can :done, Token
         can :discard, Token
       end
