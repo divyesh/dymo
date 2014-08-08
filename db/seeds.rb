cd81684fc6a4183262dffecc3faa030f3cd54bb2
Kunal Chaudhari <kunalchaudhari@gmail.com>
2014-08-08 10:31:36 +0530

    limit seeds to 5

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,8 @@
 
 # bundle exec rake db:seed
 
-Visit.where("physician_id IS NOT NULL").each do |visit|
+Visit.where("physician_id IS NOT NULL").limit(5).each do |visit|
   PhysicianVisit.create(visit_id: visit.id, physician_id: visit.physician_id)
+  puts "Visit id: #{visit.id}, Patient_id: #{visit.patient.id}"
 end
 puts "Migrated successfully. Total #{PhysicianVisit.count}"
