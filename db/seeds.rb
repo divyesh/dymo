3c88aba0558fce0c6403ae648c7d04b8b0e052a9
Kunal Chaudhari <kunalchaudhari@gmail.com>
2014-08-08 10:27:45 +0530

    instruction to run seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Daley', :city => cities.first)
+# run this with below command
+
+# bundle exec rake db:seed
 
 Visit.where("physician_id IS NOT NULL").each do |visit|
   PhysicianVisit.create(visit_id: visit.id, physician_id: visit.physician_id)
