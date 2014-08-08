87a3abac4a8c24e92f84463e083c03fcf8e49534
Kunal Chaudhari <kunalchaudhari@gmail.com>
2014-08-08 10:26:36 +0530

    seed for physician migration

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+Visit.where("physician_id IS NOT NULL").each do |visit|
+  PhysicianVisit.create(visit_id: visit.id, physician_id: visit.physician_id)
+end
+puts "Migrated successfully. Total #{PhysicianVisit.count}"
