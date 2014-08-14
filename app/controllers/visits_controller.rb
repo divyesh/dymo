b0cd1b73f1c6d64f6c85061669054854b5b9c9ab
Kunal Chaudhari <kunalchaudhari@gmail.com>
2014-08-15 00:46:41 +0530

    order test_group by group_index and order tests by index. also display 5 test_groups on same line

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -40,8 +40,8 @@ class VisitsController < ApplicationController
     @visit = @patient.visits.new
     authorize! :new, @visit
     @visit.visitdate = DateTime.now
-    @tests = Test.all.to_a
-    @test_groups = @tests.group_by { |t| t.test_group }
+    @tests = Test.all
+    @test_groups = @tests.group(:test_group).order("group_index ASC")
   end
 
   def edit
